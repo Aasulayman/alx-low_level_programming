b5ef31d997a17e45191d3e80955f467e45095f2c
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-18 08:27:52 -0800

    lowlevelC

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
  * main - A program to print a line
- * 
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
-    
