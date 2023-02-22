1bf4c594525d06cca8c30eeb0525413b8dabf9f9
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-22 09:40:51 -0800

    Nestedloops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int _abs(int) - checkfu nction that computes the absolute value
+ * @a : parameter to check the absvalue
+ * Return: Always 0.
+ */
+int _abs(int a);
+{
+if (a < 0)
+return (-a);
+else
+return (a);
+}
