29c64654b0ac3504912b696db3f628e90b98dab0
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-22 03:41:17 -0800

    Nestedloops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include  <stdioh>
 #include "main.h"
-/*
- * main - program to print
+/**
+ * main - Entry point.
+ * Description - program to print _putchar
  * return: always 0(success)
  */
 int  main(void)
 {
-putchar(c);
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
