23efeffd44d7cfec05639aa1aeaf80b731ddea6c
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-22 07:07:36 -0800

    Nestedloops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_sign - check the sign of a number.
+ *@n : parameter to check the sign of a number
+ * Return: Always 0.
+ */
+int print_sign(int n)
+if
+(n > 0)
+{
+_putchar('+');
+return (1);
+}
+elseif
+(n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+(n < 0)
+{
+_putchar('-');
+return (-1);
+}
+}
