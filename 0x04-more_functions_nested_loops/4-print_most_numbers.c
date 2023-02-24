e5c0307024d69dfe97f1d030f39826699b0222ed
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-24 15:40:50 -0800

    nestedloopings

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void print_most_numbers - check the code to print numbers
+ * 
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+{
+_putchar('0' + i);
+i++;
+}
+_putchar('\n');
+}
