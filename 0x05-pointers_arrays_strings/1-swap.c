ddfd2bb7eb26095ac2af403d39a4ad397f473848
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-27 04:41:08 -0800

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,13 +9,14 @@
  */
 void swap_int(int *a, int *b)
 {
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
+int a;
+int b;
+printf("Enter value of a: ");
+scanf("%d,&a);
+printf("Enter value of b: ");
+scanf("%d",&b);
+printf("Before swapping: a is: %d, b is: %d\n",a,b);
+swap(&a,&b);
+printf("After swapping: a is: %d, b is: %d\n",a,b);
+return (0);
 }
