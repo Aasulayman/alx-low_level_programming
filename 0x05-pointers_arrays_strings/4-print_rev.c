0c86208a7042ea9f021b1eccf7f6de32a82c6e91
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-27 07:49:51 -0800

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,17 @@
 
 /**
  * print_rev - check the code
- *
+ * @s : parameter to check
  * Return: Always 0.
  */
 void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
