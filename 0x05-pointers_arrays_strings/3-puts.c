f481e206408695aab296d2ddd3c07525280a29f4
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-27 07:31:37 -0800

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 /**
  * _puts - check the function to print a string
@@ -7,10 +8,10 @@
 
 void _puts(char *str)
 {
-  int i = 0;
-  while (*str[i] != '\0')
+int i = 0;
+while (*str[i] != '\0')
 {
-_putchar(*str[i] + 0);
+_putchar(*str[i]);
 i++;
 }
 _putchar('\n');
