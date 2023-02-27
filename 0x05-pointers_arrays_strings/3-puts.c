b444817c06b72f6cd464c82d848e4042733f6efe
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-27 07:26:01 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,10 +7,11 @@
 
 void _puts(char *str)
 {
-while (*str != '\0')
+  int i = 0;
+  while (*str[i] != '\0')
 {
-_putchar(*str + 0);
-++str;
+_putchar(*str[i] + 0);
+i++;
 }
 _putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
