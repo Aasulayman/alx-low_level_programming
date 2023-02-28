00491ffc6b32837a0d7868387e50e90b99bce4f6
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-28 06:39:43 -0800

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,11 +8,11 @@
  */
 void print_array(int *a, int n)
 {
-int b;
-for (b = 0; b < n; b++)
+int index;
+for (index = 0; index < n; index++)
 {
-prinf("%d", a[b]);
-if (b != (n - 1))
+prinf("%d", a[index]);
+if (index != (n - 1))
 {
 printf(",");
 }
