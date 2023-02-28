81f8d9c23ba469adbc2e4591c42b567bb9eaad8b
Aasulayman <aasulayman@lautech.edu.ng>
2023-02-28 05:15:31 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code to print every character of a string
+ * @str : parameter to print
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+for (int = 0; i < str[i]; i += 2)
+{
+_putchar(str[i]);
+}
+}
