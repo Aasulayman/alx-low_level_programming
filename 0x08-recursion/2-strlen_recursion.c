8064a483f93541962e20a5e866849f8f570dd518
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-08 01:45:30 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen_recursion - function that return the length of a string
- * @s: parameter to return string 
+ * @s: parameter to return string
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
