577224a32529a86dfc285a61bba8bc9bf2354499
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-08 01:04:12 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,6 @@
 
 /**
  * _puts_recursion - function that print a string
- * main - check the code
  * @s : parameter for string
  * Return: Always 0.
  */
@@ -16,10 +15,4 @@ _puts_recursion(*s + 1);
 }
 else
 _putchar('\n');
-}
-
-int main(void)
-{
-_puts_recursion("Puts with recursion");
-return (0);
 }
