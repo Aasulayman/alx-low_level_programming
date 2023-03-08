25a018c95c7a580a29f72290ffb734161d7263b1
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-08 07:08:51 -0800

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _sqrt_recursion - function to return squar root
+ * @n : parameter to determine the square root
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (_sqrt(1 n));
+}
