e747965ce88eef3309269babc64bc8c8007e6b61
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-21 14:17:54 -0700

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include dog.h
+/**
+ * init_dog - check the code
+ * @d : dod structure
+ * @name: dog char
+ * @age: dog float
+ * @owner: dog parameter
+ * Return: Always 0.
+ */
+void init_dog
+{
+  struct dog *d
+    char *name;
+  float age;
+  char *owner;
+}
