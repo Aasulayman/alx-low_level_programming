a5a6ad170365d62b24e8eed9b1205d72fcb79a9f
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-21 14:53:40 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,6 @@ float age;
 char *owner; 
 } dog;
 typedef struct dog dog_t;
-_putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
