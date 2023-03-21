3c2109c6ed47277caaa24dcad087e49b3d0dca3c
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-21 05:59:32 -0700

    processors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - fuction
+ * @argc: Parameter
+ *@argv: parameter
+* Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
