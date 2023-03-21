dd05f30eb8e57cbc7568ee17417240d74dff3da3
Aasulayman <aasulayman@lautech.edu.ng>
2023-03-21 08:20:48 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
 
 typedef struct dog
 {
@@ -8,3 +16,5 @@ typedef struct dog
   char *owner;
   
 } dog;
+typedef struct dog dog_t;
+#endif
