94116733ecaf116a3fedfb03de58dbeb48ee4411
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-04 07:48:52 -0700

    task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * listint_len - function to return number of elemnts
  * @h: head of node
@@ -9,6 +10,7 @@ size_t listint_len(const listint_t *h)
 {
 while (h)
 {
+size_t nodes = 0;
 h = h->next;
 nodes++;
 }
