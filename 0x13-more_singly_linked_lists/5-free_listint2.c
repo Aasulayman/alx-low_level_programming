13821382fd7892e27277cf99f09882362a4e7031
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-04 23:11:30 -0700

    freehead

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,3 +3,25 @@
 /**
  * free_listint2 - funtion that frees a list
  * @head: head of node
+ * Return: free
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *node;
+if (head == NULL)
+return;
+
+if (*head == NULL)
+{
+free(*head);
+head = NULL;
+return;
+}
+while (*head != NULL)
+{
+node = *head;
+*head = (*head)->next;
+free(node);
+}
+*head = NULL;
+}
