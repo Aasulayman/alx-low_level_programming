57d108e65c5e0b23a8a5af5c8a01c14650120a01
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-08 09:19:38 -0700

    fileio

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: Null terminated string to write to the file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fo, fw, lens = 0;
+if (filename == NULL)
+return (-1);
+
+fo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fo == -1)
+return (-1);
+
+if (text_content == NULL)
+text_content = "";
+while (text_content && *(text_content + lens))
+lens++;
+fw = write(fd, text_content, lens);
+if (fw == -1)
+return (-1);
+close(fw);
+return (1);
+}
