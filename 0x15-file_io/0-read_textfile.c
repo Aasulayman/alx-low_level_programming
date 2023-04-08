f89578f7c886cdf067c68d44aab431115b7a5a7f
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-08 08:40:04 -0700

    fileio

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read a text file and prints to POSIT
+ * @filename: File to open
+ * @letters: numbers of letters to read and print
+ * Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fr, fw, fo;
+char *tmp;
+if (filename == NULL)
+return (0);
+tmp = malloc(sizeof(char) * letters);
+if (tmp == NULL)
+return (0);
+fo = open(filename, 0_RDONLY);
+if (fo < 0)
+free(tmp);
+return (0);
+}
+fr = read(fo, tmp, letters);
+if (fr < 0)
+{
+free(tmp);
+return (0);
+}
+fw = write(STOUT_FILENO, tmp, fr);
+free(tmp);
+close(fo);
+
+if (fw < 0)
+return (0);
+return ((ssize_t)fw);
+}
