9a63cabe9397b076f45cfbc446a2913779212a50
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-08 09:44:46 -0700

    fileio

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fo, fw, lens;
 if (filename == NULL)
 return (-1);
-fo = open(filename, O_WRONLY | O_APPEND);
+fo = open(filename, O_RDWR | O_APPEND);
 if (fo == -1)
 return (-1);
 if (text_content == NULL)
