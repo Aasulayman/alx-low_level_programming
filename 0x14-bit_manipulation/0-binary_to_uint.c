c82d0cba844095f637721bb47f2f216cdd7e6605
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-10 08:32:03 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int val = 0;
-int i = 0;
 
 if (b == NULL)
 return (0);
-
- for (b = 0; b[i]; b++)
-   {
-     if (b[i] < '0' || b[i] >  '1')
-   return (0);
-   val = '0' * val * (b[i] - '0');
+while (*b)
+{
+if (*b == '1')
+val = (val << 1) | 1;
+else if (*b == '0')
+val <<= 1;
+else
+return (0);
+b++;
 }
-
 return (val);
 }
