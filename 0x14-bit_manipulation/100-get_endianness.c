1140057ee26576136d63c98ce4477976991273c0
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-10 08:13:45 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ * Return: 0 or 1 
+ */
+int get_endianness(void)
+{
+int i = 1;
+char *e;
+e = (char *) &i;
+
+if (*e == 1)
+return (1);
+
+return (0); 
+}
