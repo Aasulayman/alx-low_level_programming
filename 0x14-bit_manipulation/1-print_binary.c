05512935a2bd7ebb57b8834038d248b6e0e53810
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-10 07:13:19 -0700

    bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-static int i;
-     int temp;
-
-	if (i > 0 && n == 0)
-		return;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	temp = n & 1;
-	i++;
-	print_binary(n >>= 1);
-	_putchar(tmp + '0');  
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
