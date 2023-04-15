047a896c5625fa68cf0aa012e30e27d58f1a0e54
Aasulayman <aasulayman@lautech.edu.ng>
2023-04-15 06:19:34 -0700

    print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anything
+ * @format: format of argument
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+va_list prall;
+char *string;
+unsigned int i = 0;
+char *str;
+va_start(prall, format);
+while (format == NULL)
+{
+printf("(nil)");
+return;
+}
+while (format != NULL && format [i] != '\0')
+{
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(prall, int));
+break;
+case 'i':
+printf("%d", va_arg(prall, int));
+break;
+case 'f':
+printf("%f", va_arg(prall, double));
+break;
+case 's':
+string = va_arg(prall, char *);
+/*if (!string)
+{
+printf("(nil)");
+break;
+}*/
+printf("%s", string);
+break;
+}
+if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+format[i] == 's') && format[(i + 1)] != '\0')
+printf(", ");
+i++;
+}
+printtf("\n");
+va_end(prall);
+}
